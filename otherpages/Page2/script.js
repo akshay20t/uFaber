2802a93ec3bd0e4a6e609696a623e90f7a6cad44
suesanz <yadavsourav24071998@gmail.com>
2019-04-07 21:53:40 +0530

    first commit

---
+++ otherpages/Page2/script.js
@@ -0,0 +1,20 @@
+console.log(document.body.clientWidth);
+
+function openNav() {
+    if(document.body.clientWidth < 426){
+        document.getElementById("mySidenav").style.width = "310px";
+    }
+    else{
+
+        document.getElementById("mySidenav").style.width = "455px";
+        // document.body.classList.add("boxshadow");
+    }
+    document.getElementById("whole").style.opacity="0.3";
+    document.getElementById("ll").style.opacity="0.3";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("whole").style.opacity="1";
+    document.getElementById("ll").style.opacity="1";
+}
